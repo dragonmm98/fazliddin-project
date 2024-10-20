2914723e6c4dcb8c5c79bf5e2c916194a07ef9ae
dragonmm98 <3d.dragonmm@98gmail.com>
2024-10-20 21:36:43 +0900

    feat: React-Router Dom initialization

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,17 @@ import '../src/css/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Col, Container, Row } from 'react-bootstrap';
 import FormExample from './components/navbar';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Example from './components/example';
+
+
+
 
 
 function App() {
@@ -10,7 +21,34 @@ function App() {
    <Container>
     <Row>
       <Col>
-      <FormExample/>
+      <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <FormExample />
+          </Route>
+          <Route path="/">
+            <Example />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
       </Col>
     </Row>
    </Container>
